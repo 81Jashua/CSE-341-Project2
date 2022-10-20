31ccf3bd2ec0b8664b599cccde5c27ea5d70fbec
Jashua <77981430+81Jashua@users.noreply.github.com>
2022-10-20 15:27:20 -0600

    adding validation

---
+++ middleware/validate.js
@@ -0,0 +1,30 @@
+const validator = require('../helpers/validate');
+
+const saveContact = (req, res, next) => {
+  const validationRule = {
+    firstName: 'required|string',
+    lastName: 'required|string',
+    email: 'required|email',
+    birthday: 'string',
+    phone: 'string',
+    address: 'string',
+    city: 'string',
+    state: 'string',
+    zip: 'numeric'    
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+module.exports = {
+  saveContact
+};
