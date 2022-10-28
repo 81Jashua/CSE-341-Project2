f225496341062d8320bde08d9ce4e537d307ff4e
Jashua <77981430+81Jashua@users.noreply.github.com>
2022-10-28 10:31:57 -0600

    working on oath

---
+++ routes/authorization.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const petsController = require('../controllers/pets');
+const validation = require('../middleware/validate');
+
+router.get('/login', petsController.getAll);
+
+router.get('/:id', petsController.getSingle);
+
+router.post('/', validation.createUpdatePet, petsController.createPet);
+
+router.put('/:id', validation.createUpdatePet, petsController.updatePet);
+
+router.delete('/:id', petsController.deletePet);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use('/contacts', require('./contacts'));
 router.use('/pets', require('./pets'));
 router.use('/api-docs', require('./docs'));
+router.use('/authorization', require('./authorization'));
 
 module.exports = router;
